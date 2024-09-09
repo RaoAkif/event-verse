78a18533aa7b87b768b3938e3b558f63f92c9e88
RaoAkif <akifrao@gmail.com>
2024-09-10 02:11:08 +0500

    Replace with Basic Docker Configs of Hello Server!
    Remove Old Code
    Add Docker Containerized Hello Server

--- src/index.js
+++
@@ -1,9 +0,0 @@
-const app = require('./app');
-
-// Define the port from environment or fallback to 5000
-const PORT = process.env.PORT || 5000;
-
-// Start the server
-app.listen(PORT, () => {
-    console.log(`Server running on http://localhost:${PORT}`);
-});
